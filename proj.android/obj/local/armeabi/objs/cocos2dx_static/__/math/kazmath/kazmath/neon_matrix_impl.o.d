16e335d6267dceb1caf3dc529d9b82fd73634743
Doug <Doug@ChristGames.org>
2014-04-19 22:13:26 -0400

    Cocos2d-x 3.0 Upgrade

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/__/math/kazmath/kazmath/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/__/math/kazmath/kazmath/neon_matrix_impl.o: \
- C:\ChristGames\Projects\SpiritHeroes\proj.android\../cocos2d/cocos/2d/../math/kazmath/kazmath/neon_matrix_impl.c \
- C:\ChristGames\Projects\SpiritHeroes\proj.android\../cocos2d/cocos/2d/../math/kazmath/kazmath/neon_matrix_impl.h
-
-C:\ChristGames\Projects\SpiritHeroes\proj.android\../cocos2d/cocos/2d/../math/kazmath/kazmath/neon_matrix_impl.h:
